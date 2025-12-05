0e77a088f0f841c90f8f8dbfa286d9702c7f462e
Zic0 <146777021+Zyad-Ehab@users.noreply.github.com>
2025-12-05 18:47:17 +0200

    Delete utils directory

--- utils/config.dart
+++
@@ -1,4 +0,0 @@
-class Config {
-  static const String apiKey = '47ecee72a31ac60126f231c61c7f9df9';
-  static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
-}
--- utils/constants.dart
+++
@@ -1,5 +0,0 @@
-class AppConstants {
-  static const String appName = 'Weather App';
-  static const String defaultCity = 'London';
-  static const Duration apiTimeout = Duration(seconds: 10);
-}
